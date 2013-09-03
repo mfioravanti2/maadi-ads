1ec277a01c4727833cf4edf72c032027582b7a75
Mr. Fluffy Pants <shull2013@my.fit.edu>
2013-09-03 16:21:54 -0400

    Still working on popen3 with ruby.  Apparently its very difficult to read the entire buffer without causing hangups for stdout and stderr.  I was able to get the stdout to work IF I knew all the output ahead of time.  If I read one extra line past the buffer, it hands indefinetely (until the program closes).  This is problematic.  Will investigate further.
    SFH

--- utils/java/temporary.rb
+++ utils/java/temporary.rb
@@ -4,30 +4,56 @@ class Temporary
 
   #First, get the name of the file
   @fileName = 'bsh-2.0b4.jar'
-  @filePath = Dir.pwd
-  @file = @filePath + '/' +  @fileName
-  p @file
+
   #run the program
-  #@commandName = 'java -jar ' + @fileName
-  #The method below assumes that the bsh file is in jre/lib/ext for ease of access
-  #@commandName = 'java bsh.Interpreter'
-  @commandName = 'java -cp ' + @file  + ' bsh.Interpreter'
-  #io = IO.popen(@commandName, 'r+')
-  #$stdout = io
-  #io.write 'Stack stack = new Stack();\n'
-  #io.write 'System.out.println(stack.size());\n'
-  #$stdout = IO.new 0
-  #p io.read(1)
-  #io.close
+  @commandName = 'java -cp ' + @fileName  + ' bsh.Interpreter'
 
-  stdin, stdout, stderr = Open3.popen3(@commandName)
-  @commandExec  = 'Stack stack = new Stack(); System.out.println(stack.size());'
-  #stdin.print(@commandExec )
-  stdin.print @commandExec
-  stdin.close()
-  p stdout.readlines()
-  p stderr.read()
+  stdin, stdout, stderr, wait_thr= Open3.popen3(@commandName)
+  @commandExec  = "Stack stack = new Stack();\n System.out.println(\"Stack size: \" + stack.size());\n stack.add(1);\n System.out.println(\"Stack size: \" + stack.size());\n"
+  p 'Execute program'
+  stdin.print( @commandExec)
+  stdin.flush()
 
+  p 'Print Standard Output'
+  #@output = stdout.readline;
+
+  begin
+    stdout.each { |line| print line }
+  rescue Errno::EIO
+  end
+
+
+  p 'Print Standard Error'
+  @output = stderr.gets
+  while @output != nil
+
+    p 'Output: ' + @output
+    @output = stderr.gets
+  end
+
+  #Try again
+
+
+  p 'Execute program'
+  #Write Execution program
+  @commandExec  = "stack.add(2);\n System.out.println(\"Stack size: \" + stack.size());\n"
+  stdin.print( @commandExec)
+  stdin.flush()
+
+  p 'Print Standard Output'
+  @output = ''
+  while @output != nil
+    output = stdout.gets
+    p output
+  end
+
+
+  p 'Print Standard Error'
+  @output = ''
+  while @output != nil
+    output = stderr.gets
+    p output
+  end
 
 
 end
