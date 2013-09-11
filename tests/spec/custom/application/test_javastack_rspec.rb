86f179831931487a380275b4629d8dbc8f094a5f
Mr. Fluffy Pants <shull2013@my.fit.edu>
2013-09-11 13:08:21 -0400

    Made stuff work (hows that for a meaningful message!)
    Just kidding, got the tester to work with Push, Pop, Create, and size to work.  I think atIndex works.
    
    Tester updated.
    
    Removed lots of crappy print statements.  Kept a few meaningful print statements, but used the maadi's post_message system.
    
    SFH

--- tests/spec/custom/application/test_javastack_rspec.rb
+++ tests/spec/custom/application/test_javastack_rspec.rb
@@ -15,6 +15,12 @@ require_relative '../../../../lib/core/procedure/procedure'
 require_relative '../../../../lib/core/procedure/results'
 require_relative '../../../../lib/core/procedure/parameter'
 
+def build_step( test, look_for, command, on_failure )
+  parameters = Array.new
+  step = Maadi::Procedure::Step.new(test + '', 'application', look_for, command, parameters, on_failure)
+  return step
+end
+
 describe Maadi::Application::JavaStack do
   it 'Checks the default parameters stored on JavaStack class when instantiated: BSHPATH' do
     javaStack = Maadi::Application::JavaStack.new()
@@ -42,20 +48,28 @@ describe Maadi::Application::JavaStack do
     parameters = Array.new()
     #The first step is to create a stack.  Do not pass any parameters OR an execution string,
     #as the execution string is taken care of in the switch statement
-    step1 = Maadi::Procedure::Step.new('NULCONSTRUCT', '','COMPLETE', 'NULCONSTRUCT', parameters, 'CONTINUE')
+    step1 = build_step('NULCONSTRUCT', 'COMPLETE','', 'TERM-PROC')
 
     #The second step is to get the size.  This should be 0
-    step2 = Maadi::Procedure::Step.new('SIZE', '','COMPLETE', 'SIZE', parameters, 'CONTINUE')
+    step2 = build_step('SIZE', 'COMPLETE','', 'TERM-PROC')
 
     #The third step, add a value to the operation
-    #parameter1 = Maadi::Procedure::Parameter.new('VALUE', )
-    #step3 = Maadi::Procedure::Step.new('PUSH', '', 'COMPLETE', 'PUSH', parameters, 'CONTINUE')
+    step3 = build_step('PUSH', '[LVALUE]', '', 'TERM-PROC' )
+    constraint =  Maadi::Procedure::ConstraintRangedInteger.new( 1, 1024 )
+    step3.parameters.push Maadi::Procedure::Parameter.new('[RVALUE]', constraint )
+    step3.parameters[0].populate_value
+
+    #The forth step, pop a vlaue
+    step4 = build_step('POP', '[LVALUE]', '', 'TERM-PROC' )
+
 
     #create the procedure
     procedure1 = Maadi::Procedure::Procedure.new(55)
     #Add the steps to the procedure
     procedure1.add_step(step1)
     procedure1.add_step(step2)
+    procedure1.add_step(step3)
+    procedure1.add_step(step4)
 
     results = javaStack.execute(1, procedure1);
 
