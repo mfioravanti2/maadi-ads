20d9bca829d1476b460ef55c6a25a204c8a5e4bf
Mr. Fluffy Pants <shull2013@my.fit.edu>
2013-09-26 15:35:56 -0400

    Started work on Alebraic ads stack.  For some reason, it formatted my class name stupidly.  I want it in CAMELCASE.  But it did it in underscore case and wont change it.
    Stupid ruby mime issue
    
    SFH.

---
+++ lib/custom/expert/algebraic_ads_stack.rb
@@ -0,0 +1,207 @@
+# Author : Scott Forest Hull II (shull2013@my.fit.edu)
+#          Florida Institute of Technology
+# Course : CSE5400 Special Topics - High Volume Automated Testing
+# Date   : 09/26/2013
+# File   : AlgebraicADSStack.rb
+#
+# Summary: This is an extension of the ADSStack class that utilizes
+#          various tests for axioms listed.
+require_relative 'factory'
+require_relative '../../core/helpers'
+require_relative '../procedure/ConstraintConstant'
+require_relative '../procedure/ConstraintSingleWord'
+require_relative '../procedure/ConstraintSingleString'
+require_relative '../procedure/ConstraintPickList'
+require_relative '../procedure/ConstraintMultiPickList'
+require_relative '../procedure/ConstraintMultiWord'
+require_relative '../procedure/ConstraintRangedInteger'
+require_relative 'ADSStack'
+
+module Maadi
+  module Expert
+    class AlgebraicADSStack < ADSStack
+
+      def initialize
+        super('AlgebraicADSStack')
+
+        #Options originally from  ADSStack are taken care of above.
+        #Will need extra specification for experts.
+
+        #axiom: stack = stack.pop(stack.push(Object))
+        @options['PUSHPOP_RATIO'] = 1
+
+        #axiom: stack.size() = stack.pop(stack.push(Object)).size()
+        @options['PUSHPOPSIZE_RATIO'] = 1
+
+        #axoim:  (new stack()).index(0) = error
+        @options['NEWSTACKINDEX_RATIO'] = 1
+
+        #axoim: (new stack()).size() = 0
+        @options['NEWSTACKSIZE_RATIO'] = 1
+
+      end
+
+      # prepare will setup the execution environment.  No tests will be executed but all required
+      # resources and services will be prepared to execution.
+      # return (bool) true if all of the components are read.
+      def prepare
+        @tests = Array.new
+
+        items = %w(PUSHPOP PUSHPOPSIZE NEWSTACKINDEX NEWSTACKSIZE)
+        items.each do |item|
+          if @options["#{item}_RATIO"].to_i >= 1
+            1.upto(@options["#{item}_RATIO"].to_i) do
+              @tests.push item
+            end
+          end
+        end
+
+        #This will add other tests as well based on ratios listed in ADSSTACK
+        super
+      end
+
+      # build/update a procedure for a specified test
+      #   this function will mark a procedure as complete when all of the possible options
+      #   have been specified.  Procedures built are assumed to have been successfully executed
+      #   on an application so the state model will reflect any changes or updates that are
+      #   expected to occur
+      # test (String) name of a test build or update
+      # procedure (Maadi::Procedure::Procedure) test procedure to be modified
+      #                                         setting the value to nil, will cause a new procedure
+      #                                         to be instantiated
+      # return (Maadi::Procedure::Procedure) the procedure that was constructed based on current information
+      def procedure( test, procedure )
+        unless is_procedure?( procedure )
+          procedure = build_skeleton( test )
+        end
+
+        case test.downcase
+          when 'pushpop'
+            return manage_pushpop( procedure )
+          when 'pushpopsize'
+            return manage_pushpopsize( procedure )
+          when 'newstackindex'
+            return manage_newstackindex( procedure )
+          when 'newstacksize'
+            return manage_newstacksize( procedure )
+          else
+        end
+
+        return procedure
+      end
+
+      #build a Stack Push Pop procedure
+      def manage_pushpop (procedure)
+
+        unless is_procedure?( procedure )
+          return procedure
+        end
+
+        case procedure.id
+          when 'PUSHPOP-NEW'
+            reutrn build_pushpop_new('PUSHPOP-LAST')
+          when 'PUSHPOP-LAST'
+            return build_pushpop_finalize( procedure, procedure.step )
+          else
+        end
+
+        return procedure
+
+
+      end
+
+      def build_pushpop_new( next_step )
+        procedure = build_skeleton( 'PUSHPOP' )
+        step1 = build_step('PUSH', 'LVALUE', '', 'TERM-PROC' )
+        step2 = build_step('POP', 'LVALUE', '', 'TERM-PROC' )
+
+        constraint =  Maadi::Procedure::ConstraintRangedInteger.new( 1, @options['MAX_INTEGER'] )
+        step1.parameters.push Maadi::Procedure::Parameter.new('[RVALUE]', constraint )
+
+        procedure.add_step( step1 )
+        procedure.add_step( step2 )
+        procedure.id = next_step
+
+        return procedure
+      end
+
+      def build_pushpop_finalize( procedure, steps )
+        unless is_procedure?( procedure ) and is_step?( step )
+          return procedure
+        end
+
+        steps[0].id = 'PUSH'
+        steps[1].id = 'POP'
+        procedure.id = 'PUSHPOP'
+
+        rvalue = step[0].get_parameter_value( '[RVALUE]' )
+        if rvalue != ''
+          procedure.done
+        else
+          procedure.failed
+        end
+
+        return procedure
+      end
+
+      #build a Stack Push Pop Size procedure
+      def manage_pushpopsize (procedure)
+
+        unless is_procedure?( procedure )
+          return procedure
+        end
+
+        case procedure.id
+          when 'PUSHPOPSIZE-NEW'
+            reutrn build_pushpopsize_new('PUSHPOPSIZE-LAST')
+          when 'PUSHPOPSIZE-LAST'
+            return build_pushpopsize_finalize( procedure, procedure.step )
+          else
+        end
+
+        return procedure
+
+
+      end
+
+      def build_pushpopsize_new( next_step )
+        procedure = build_skeleton( 'PUSHPOPSIZE' )
+        step1 = build_step('PUSH', 'LVALUE', '', 'TERM-PROC' )
+        step2 = build_step('POP', 'LVALUE', '', 'TERM-PROC' )
+        step3 = build_step('SIZE', 'LVALUE', '', 'TERM-PROC' )
+
+        constraint =  Maadi::Procedure::ConstraintRangedInteger.new( 1, @options['MAX_INTEGER'] )
+        step1.parameters.push Maadi::Procedure::Parameter.new('[RVALUE]', constraint )
+
+        procedure.add_step( step1 )
+        procedure.add_step( step2 )
+        procedure.add_step( step3 )
+        procedure.id = next_step
+
+        return procedure
+      end
+
+      def build_pushpopsize_finalize( procedure, steps )
+        unless is_procedure?( procedure ) and is_step?( step )
+          return procedure
+        end
+
+        steps[0].id = 'PUSH'
+        steps[1].id = 'POP'
+        steps[2].id = 'SIZE'
+        procedure.id = 'PUSHPOPSIZE'
+
+        rvalue = step[0].get_parameter_value( '[RVALUE]' )
+        if rvalue != ''
+          procedure.done
+        else
+          procedure.failed
+        end
+
+        return procedure
+      end
+
+
+    end
+  end
+end
