40f38aef37a30d6ca0220708575d971429a70e52
Mark Fioravanti <mark.fioravanti.ii@ecultist.com>
2013-10-02 18:32:09 -0400

    Updated AddConstraint to use ConstraintRangedInteger

--- bin/builder-cli.rb
+++ bin/builder-cli.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
 
 require_relative '../lib/core/expert/expert'
+require_relative '../lib/custom/expert/ADSStack'
+
+expert = Maadi::Expert::ADSStack.new('ADSStack')
 
 builder = Maadi::Expert::Builder::Builder.new()
 builder.set_option('USE-BUILDER', 'TRUE')
@@ -8,7 +11,7 @@ builder.set_option('BUILD-NAME', 'ADSStack')
 builder.set_option('USE-MODEL', 'FALSE')
 builder.prepare
 
-procedure = builder.procedure( 'PUSH', nil, nil, nil )
+procedure = builder.procedure( 'PUSH', nil, expert, nil )
 
 if procedure != nil
   puts "Procedure named #{procedure.id} was created."
@@ -18,13 +21,13 @@ if procedure != nil
     step.parameters.each do |parameter|
       puts "\t\tPARAMETER: #{parameter.label}"
       if parameter.constraint != nil
-        puts "\t\t\tCONSTRAINT: #{parameter.constraint.type}"
+        puts "\t\t\tCONSTRAINT: #{parameter.constraint.display}"
       end
     end
   end
 end
 
-procedure = builder.procedure( 'CREATE',procedure, nil, nil )
+procedure = builder.procedure( 'CREATE',procedure, expert, nil )
 
 if procedure != nil
   puts "Procedure named #{procedure.id} was updated."
@@ -34,7 +37,7 @@ if procedure != nil
     step.parameters.each do |parameter|
       puts "\t\tPARAMETER: #{parameter.label}"
       if parameter.constraint != nil
-        puts "\t\t\tCONSTRAINT: #{parameter.constraint.type}"
+        puts "\t\t\tCONSTRAINT: #{parameter.constraint.display}"
       end
     end
   end
--- lib/core/expert/builder/addconstraint.rb
+++ lib/core/expert/builder/addconstraint.rb
@@ -31,12 +31,21 @@ module Maadi
                     if parameter.label == @to_parameter
                       case @type.downcase
                         when 'ranged-integer'
-                          min_value = @node['min_range']
-                          if min_value.gsub( /OPTION*/ )
 
+                          min_value = @node['min_range']
+                          if min_value.include?( 'OPTIONS:' ) && expert != nil
+                            key = min_value.sub( 'OPTION:', '' ).to_s
+                            min_value = expert.get_option( key )
                           end
+
                           max_value = @node['max_range']
-                          constraint = Maadi::Procedure::ConstraintRangedInteger.new(  )
+                          if max_value.include?( 'OPTIONS:' ) && expert != nil
+                            key = max_value.sub( 'OPTIONS:', '' ).to_s
+                            max_value = expert.get_option( key )
+                          end
+
+                          constraint = Maadi::Procedure::ConstraintRangedInteger.new( min_value, max_value  )
+                          parameter.constraint = constraint
                         else
                       end
                     end
