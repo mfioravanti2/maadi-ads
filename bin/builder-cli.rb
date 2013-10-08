f50f44dfbf872f302930b0cb95c540e2576171bf
Mark Fioravanti <mark.fioravanti.ii@ecultist.com>
2013-10-08 11:48:06 -0400

    Added POP to Expert::Builder::ADSStack

--- bin/builder-cli.rb
+++ bin/builder-cli.rb
@@ -11,7 +11,7 @@ builder.set_option('BUILD-NAME', 'ADSStack')
 builder.set_option('USE-MODEL', 'FALSE')
 builder.prepare
 
-test = 'PUSH'
+test = 'POP'
 
 procedure = builder.procedure( test, nil, expert, nil )
 
@@ -19,7 +19,7 @@ if procedure != nil
   puts "Procedure named #{procedure.id} was created."
 
   procedure.steps.each do |step|
-    puts "\tSTEP: #{step.id}"
+    puts "\tSTEP: #{step.id} (Looking for #{step.look_for})"
     step.parameters.each do |parameter|
       puts "\t\tPARAMETER: #{parameter.label} = #{parameter.value}"
       if parameter.constraint != nil
@@ -29,7 +29,9 @@ if procedure != nil
   end
 end
 
-procedure.get_step( 'PUSH-WIP' ).get_parameter( '[RVALUE]' ).populate_value
+if test == 'PUSH'
+  procedure.get_step( 'PUSH-WIP' ).get_parameter( '[RVALUE]' ).populate_value
+end
 
 procedure = builder.procedure( test, procedure, expert, nil )
 
@@ -37,7 +39,7 @@ if procedure != nil
   puts "Procedure named #{procedure.id} was updated."
 
   procedure.steps.each do |step|
-    puts "\tSTEP: #{step.id}"
+    puts "\tSTEP: #{step.id} (Looking for #{step.look_for})"
     step.parameters.each do |parameter|
       puts "\t\tPARAMETER: #{parameter.label} = #{parameter.value}"
       if parameter.constraint != nil
