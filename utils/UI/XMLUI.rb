35fc50e5d1839540ecfe0a09880fae1b969be667
FasterFluffy <shull2013@my.fit.edu>
2013-10-10 01:24:00 -0400

    Pushing (empty) classes to start exploring UI pieces for ruby.
    SFH.

---
+++ utils/UI/XMLUI.rb
@@ -0,0 +1,23 @@
+
+
+module Application
+  module UI
+    class XMLUI
+
+
+      def initialize (xMLObject)
+
+        #Takes an xmlObject and creates a UI piece from it.
+        @xmlObject = xMLObject
+        #Returns a UI piece
+
+      end
+
+      def createUI
+
+      end
+
+
+    end
+  end
+end
