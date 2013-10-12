449235b24dbe8db3d785326e3c74824ab4d7db86
FasterFluffy <shull2013@my.fit.edu>
2013-10-12 19:33:39 -0400

    Saving github related issue:
    https://github.com/ashbb/green_shoes/issues/80#
    
    Hopefully this is a case of me doing something wrong and not a bug.  If this is a bug that can't be fixed, we might be SOL on using shoes with ruby.
    
    SFH

--- utils/UI/UITemporary.rb
+++ utils/UI/UITemporary.rb
@@ -2,29 +2,17 @@
 require 'green_shoes'
 require 'nokogiri'
 
-class UITemporary < Shoes::Widget
-  def initialize
-    stack stroke: red, fill: blue do
-      para 'stuff'
-      r = star(80, 65, 5, 60, 40)
-      r.style(stroke: blue, fill: black)
-      star(50, 60, 5, 25, 10)
-      star(110, 60, 5, 25, 10)
-      a = arc(80, 100, 100, 20, 90, 180)
+
+Shoes.app :width => 1000, :height => 500 do
+
+  flow :margin_left => 10 do
+    para "XMLElementName"
+    edit_line "A"
+    edit_line "B"
+
+    flow :margin_left => 10 do
+      edit_line "C"
     end
   end
-end
-
-Shoes.app do
-
-  fXML = File.open( "test.xml" )
-  @xMLObject = Nokogiri::XML(fXML)
-  fXML.close
-
-  flow do
-    @ui = UITemporary
-  end
-
-
 
 end
